914425706104dd813d614903b3932a808a7f4fcf
FAlooC <pratyush.singh1@oyorooms.com>
2020-07-21 01:04:49 +0530

    added content

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,20 +3,22 @@ import Divider from './divider'
 
 const Footer = () => {
     return (
-        <footer id="footer" className="bg-gray p-md-4 p-3 text-center footer">
-            <Divider className="bg-gray-dark" />
-            <h2 className="text-light">Contact Us</h2>
-            <Divider className="bg-gray-dark" />
-            <div className="p-lg-3 p-2">
-                <a href="mailto:support@shiprakway.com" className="text-light">
-                    <img className="mx-lg-3 mx-2" src={process.env.PUBLIC_URL + '/img/mail.svg'} alt="mail icon" />
-                    support@shiprakway.com
-                </a>
+        <footer id="footer" className="bg-gray px-md-5 pt-lg-5 p-3 text-center footer">
+            <div className="container">
+                <Divider className="bg-gray-dark" />
+                <h2 className="text-light">Contact Us</h2>
+                <Divider className="bg-gray-dark" />
+                <div className="p-lg-3 p-2">
+                    <a href="mailto:support@shiprakway.com" className="text-light">
+                        <img className="mx-lg-3 mx-2" src={process.env.PUBLIC_URL + '/img/mail.svg'} alt="mail icon" />
+                        support@shiprakway.com
+                    </a>
+                </div>
+                <p className="text-light p-lg-3 p-2">
+                    <img className="mx-lg-3 mx-2" src={process.env.PUBLIC_URL + '/img/phone.svg'} alt="mail icon" />
+                    +91 9850512972
+                </p>
             </div>
-            <p className="text-light p-lg-3 p-2">
-                <img className="mx-lg-3 mx-2" src={process.env.PUBLIC_URL + '/img/phone.svg'} alt="mail icon" />
-                +91 9850512972
-            </p>
         </footer>
     )
 }
--- src/components/home.js
+++ src/components/home.js
@@ -19,7 +19,7 @@ const Home = () => {
                 alt="down-arrow"
                 onClick={handleClick}
             />
-            <section className="px-md-5 px-2">
+            <section className="px-md-5 px-3">
                 <div className="container">
                     <div className="row">
                         <div className="col-md-4 col-12 py-md-5 py-3">
@@ -50,11 +50,11 @@ const Home = () => {
                     allowFullScreen
                 ></iframe>
             </div>
-            <section className="px-md-5 px-2">
+            <section className="px-md-5 px-3">
                 <div className="container">
                     <div className="row">
                         <div className="col-12 py-3">
-                            <Divider className="bg-light" />
+                            <Divider className="bg-light mt-5" />
                             <h2 className="text-primary heading text-center">
                                 Your growth and performance excellence partner
                             </h2>
@@ -84,6 +84,64 @@ const Home = () => {
                             </p>
                         </div>
                     </div>
+                </div>
+            </section>
+            <section className="px-md-5 px-3 mb-5">
+                <div className="container">
+                    <Divider className="bg-light mb-5" />
+                    <p className="content">
+                        Based in the city of Bengaluru, we provide superior services in the area of Operations.
+                    </p>
+                    <p className="content">
+                        During the manufacturing cycle of a PET product, it goes through various stages before it is
+                        ready to be sold in a commercial form. Among these multiple stages, a few are generally
+                        overlooked. A detailed study reveals that because of this, the actual manufacturing cost is much
+                        more than what was initially projected. This underestimation leads to higher budgeted
+                        expenditure for the particular product.
+                    </p>
+                    <p className="content">ShiprakWay can help you overcome this major issue and improve your OEE.</p>
+                    <h2 className="text-primary heading mt-5 w-mc">
+                        Our Domain
+                        <Divider className="bg-light" />
+                    </h2>
+                    <ol type="a" className="content text-left">
+                        <li className="mt-3">
+                            Consulting in the areas of Restructuring, Transaction Advisory, Process re-engineering,
+                            Production Management, Product development
+                        </li>
+                        <li className="mt-3">Compliances in the areas of productivity and cost control</li>
+                        <li className="mt-3">
+                            Mentoring various projects at large, with the knowledge set to keep up with the constant
+                            change in technology
+                        </li>
+                    </ol>
+                    <h2 className="text-primary heading mt-5 w-mc">
+                        Our Goal
+                        <Divider className="bg-light" />
+                    </h2>
+                    <p className="content mt-3">
+                        Our goal is to exceed the expectations of every client by offering outstanding customer service,
+                        increasing flexibility and value, thus optimizing organizational functionality and raising
+                        efficiency.
+                    </p>
+                    <h2 className="text-primary heading mt-5 w-mc">
+                        Our Knowledge Bank
+                        <Divider className="bg-light" />
+                    </h2>
+                    <p className="content mt-3">
+                        Our functional and technical expertise, combined with hands-on experience, ensures that our
+                        clients receive the most effective and professional service. We accomplish our goals by
+                        utilizing the skills of a unique team of seasoned professionals in the fields of Organisational
+                        and Leadership Development, Production Management, Strategic Planning, and Operational
+                        Execution. Companies continuously want to grow and evolve, and we help them by sharing our
+                        expertise and essential tools in making those successful transitions.
+                    </p>
+                    <p className="content mt-3">
+                        To achieve our goals, we need to network with companies and people that have a passion for
+                        success and are looking to take the next step. We would like to partner with your company to
+                        help you create the most profitable, flexible, sustainable, and supportive organization
+                        possible.
+                    </p>
                 </div>
             </section>
         </React.Fragment>
